6d964a0b776126a7fa4edc51d7250e6d5b2ffe95
rmyan-yk <rmyan@cps.edu>
2025-01-16 22:58:47 -0600

    Update and rename Rock, Paper, Scissors.py to Rock, Paper, Scissors Project #3/Rock, Paper, Scissors.py

--- Rock, Paper, Scissors Project #3/Rock, Paper, Scissors.py
+++ Rock, Paper, Scissors Project #3/Rock, Paper, Scissors.py
@@ -1,9 +1,4 @@
-### Project 3: Rock, Paper, Scissors
-#**Summary**: This program allows users to play Rock, Paper, Scissors against the computer
-#**Key Features**: 
-  #Uses random to determine whether the computer chooses "Rock," "Paper," or "Scissors"
-  #Keeps track of player wins and losses
-  #Uses "if" statements to determine a winner from user and computer input
+#Rock, Paper, Scissors
 
 #Innit
 import random
